6377f8b6ad75b1bb6ddc760be4d9668bfde8a09a
Kathryn Carr and Ron Park <kathrynelizabethcarr@gmail.com>
2015-02-24 11:17:51 -0800

    Update HTML page

--- services/AnimalsFactory.js
+++ services/AnimalsFactory.js
@@ -1,9 +1,9 @@
 animalShelter.factory('AnimalsFactory', function AnimalsFactory() {
   var factory = {};
-  factory.animals = [];
+  factory.animals = [{name: "Ron", type: "Dog", years: 1, months: 8, adopted: false}];
 
   factory.addAnimal = function() {
-    var animal = {name: factory.animalName, type: factory.animalType, adopted: false, age: factory.animalAge};
+    var animal = {name: factory.animalName, type: factory.animalType, years: factory.animalYears, months: factory.animalMonths, adopted: false};
     factory.animals.push(animal);
     factory.animalName = null;
     factory.animalType = null;
